e5cadb7b36635e1a71af8e8126b4c41c8bd9a015
Invincible-cmd <boolbiz123@gmail.com>
2025-02-15 16:48:36 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,6 +24,7 @@ function setup() {
   selectEpisode.option("Episode 8");
 
   selectEpisode.changed(content);
+  selectSeason.changed(content);
 
 }
 function content() {
@@ -48,7 +49,6 @@ function draw() {
   background(20);
   fill(40);
   rect(windowWidth/2,windowHeight/2 + 5,windowWidth / 1.2 + 50,windowHeight / 1.2 + 50, 20, 20, 20, 20)
-  content()
 }
 
 
